c539fc2f6fdad198fcf41e93a4ae35239c65ad5a
BugInMyHEAD <buginmyhead@gmail.com>
2019-07-02 11:48:20 +0900

    init

---
+++ 쇠막대기/쇠막대기.cpp
@@ -0,0 +1,31 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42585
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    int left_paren = 0;
+    bool last_was_left_paren = false;
+    for (const auto& ch : arrangement) {
+        switch (ch) {
+        case '(' :
+            ++left_paren;
+            last_was_left_paren = true;
+        break;
+
+        case ')' :
+            --left_paren;
+            if (last_was_left_paren) {
+                answer += left_paren;
+                last_was_left_paren = false;
+            } else {
+                ++answer;
+            }
+        break;
+        }
+    }
+    return answer;
+}
