d4955df22fda14f757ace940790cfb231dc5a329
BugInMyHEAD <buginmyhead@gmail.com>
2019-07-03 05:26:49 +0900

    format

--- 쇠막대기/쇠막대기.cpp
+++ 쇠막대기/쇠막대기.cpp
@@ -3,29 +3,27 @@
 #include <string>
 #include <vector>
 
-using namespace std;
+int solution(std::string arrangement) {
+  int answer = 0;
+  int left_paren = 0;
+  bool last_was_left_paren = false;
+  for (const auto& ch : arrangement) {
+    switch (ch) {
+      case '(' :
+        ++left_paren;
+        last_was_left_paren = true;
+      break;
 
-int solution(string arrangement) {
-    int answer = 0;
-    int left_paren = 0;
-    bool last_was_left_paren = false;
-    for (const auto& ch : arrangement) {
-        switch (ch) {
-        case '(' :
-            ++left_paren;
-            last_was_left_paren = true;
-        break;
-
-        case ')' :
-            --left_paren;
-            if (last_was_left_paren) {
-                answer += left_paren;
-                last_was_left_paren = false;
-            } else {
-                ++answer;
-            }
-        break;
+      case ')' :
+        --left_paren;
+        if (last_was_left_paren) {
+            answer += left_paren;
+            last_was_left_paren = false;
+        } else {
+            ++answer;
         }
+      break;
     }
-    return answer;
+  }
+  return answer;
 }
