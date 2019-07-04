799db7cb19778fc66f05a819cc66c43e1521a1a2
BugInMyHEAD <buginmyhead@gmail.com>
2019-07-05 04:51:40 +0900

    implement 가장 큰 수_v2

--- 가장 큰 수/가장 큰 수_v2.cpp
+++ 가장 큰 수/가장 큰 수_v2.cpp
@@ -8,31 +8,41 @@
 #include <algorithm>
 #include <sstream>
 
+static bool IsGreaterNumberString(const std::string& a, const std::string& b) {
+  size_t a_substr_from = 0, b_substr_from = 0;
+  for (
+    size_t a_idx = 0, b_idx = 0;
+    a_idx < a.size() || b_idx < b.size();
+    ++a_idx, ++b_idx
+  ) {
+    if (a_idx >= a.size()) {
+      b_substr_from = b_idx;
+      a_idx = a_substr_from;
+    } else if (b_idx >= b.size()) {
+      a_substr_from = a_idx;
+      b_idx = b_substr_from;
+    }
+    if (a[a_idx] > b[b_idx]) {
+      return true;
+    } else if (a[a_idx] < b[b_idx]) {
+      return false;
+    }
+  }
+  return false;
+}
+
 std::string solution(std::vector<int> numbers) {
   std::vector<std::string> number_strings;
   for (int el : numbers) {
     number_strings.emplace_back(std::to_string(el));
   }
-  std::sort(
-    number_strings.begin(), number_strings.end(),
-    [](const std::string& a, const std::string& b) -> bool {
-      size_t longer = std::max(a.size(), b.size());
-      for (size_t i5 = 0, j5 = 0, k5 = 0; k5 < longer; ++i5, ++j5, ++k5) {
-        i5 %= a.size();
-        j5 %= b.size();
-        if (a[i5] > b[j5]) {
-          return true;
-        } else if (a[i5] < b[j5]) {
-          return false;
-        }
-      }
-      return false;
-    }
-  );
-  std::ostringstream ss;
+  std::sort(number_strings.begin(), number_strings.end(), IsGreaterNumberString);
+  
   if (number_strings[0][0] == '0') {
-    ss << "0";
-  } else for (const auto& el : number_strings) {
+    return "0";
+  }
+  std::ostringstream ss;
+  for (const auto& el : number_strings) {
     ss << el;
   }
   return ss.str();
