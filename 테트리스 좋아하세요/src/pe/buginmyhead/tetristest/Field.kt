c4ec27b0e7828cc45bb3b346d45f39ac4c22eae0
BugInMyHEAD <buginmyhead@gmail.com>
2019-07-10 10:16:42 +0900

    add class Column

--- 테트리스 좋아하세요/src/pe/buginmyhead/tetristest/Field.kt
+++ 테트리스 좋아하세요/src/pe/buginmyhead/tetristest/Field.kt
@@ -3,11 +3,28 @@ package pe.buginmyhead.tetristest
 class Field(
   val width: Int, val height: Int
 ) {
-  private val arr = IntArray(width)
-  val ar
-    get() = arr.asList()
+  private val array = IntArray(width)
+  val ar get() = array.asList()
 
   fun pileUp(blockCreation: BlockCreation) {
 
+  }
+
+  private class Column(
+    height: Int
+  ) {
+    private val array = BooleanArray(height)
+    val height get() = array.size
+    var highestBlockPosition = 0
+
+    fun draw(position: Int) {
+      if (array[position]) {
+        throw NoSuchElementException()
+      }
+      array[position] = true
+      if (position > highestBlockPosition) {
+        highestBlockPosition = position
+      }
+    }
   }
 }
