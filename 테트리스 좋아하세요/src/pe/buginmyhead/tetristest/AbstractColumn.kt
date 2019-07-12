61a84a1db7ac7beabcd2c61079346ae792413d7c
BugInMyHEAD <buginmyhead@gmail.com>
2019-07-12 10:59:07 +0900

    format

--- 테트리스 좋아하세요/src/pe/buginmyhead/tetristest/AbstractColumn.kt
+++ 테트리스 좋아하세요/src/pe/buginmyhead/tetristest/AbstractColumn.kt
@@ -7,15 +7,18 @@ abstract class AbstractColumn(
   height: Int
 ) {
   protected val array = BooleanArray(height)
+
   /**
    * The maximum height
    */
   val size: Int
     get() = array.size
+
   /**
    * The lowest position you can pile up a block piece
    */
   var height = 0
     protected set
+
   operator fun get(index: Int) = array[index]
 }
